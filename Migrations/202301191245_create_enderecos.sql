211141040f01d67c07918b16d68e66b7baf03cc9
candided <edson.candido@howden.com>
2023-01-19 12:58:54 -0300

    added model endereco

---
+++ Migrations/202301191245_create_enderecos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE enderecos (
+    Id INTEGER PRIMARY KEY,
+    Rua TEXT NOT NULL,
+    Numero INTEGER NOT NULL,
+    Bairro TEXT NOT NULL,
+    Cidade TEXT NOT NULL,
+    Estado TEXT NOT NULL
+);
